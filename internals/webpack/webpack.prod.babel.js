165f22eece13dde0760dc7feb314534f81367395
Deepanshu Bansal <bansaldeepanshu08@gmail.com>
2021-07-25 19:10:46 +0530

    Changed app name

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -119,9 +119,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'Checklist',
+      short_name: 'Todos',
+      description: 'Todo checklist tracking tasks',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
